3d54f8019c3724366fcf5b35de43621ce5ba95cb
Diomar-s <diomarlugo405@gmail.com>
2024-10-17 20:01:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Kakaroto-Bot', {
+say('Vegeta-Bot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • legendary`, {
+say(`Developed By • Saiyan`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
