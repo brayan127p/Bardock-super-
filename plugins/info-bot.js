0ecf3f6cf1dbb0ee7e2e9ac75fce577ef4812117
Niño Piña <ninopina10@gmail.com>
2024-10-26 17:51:42 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -9,10 +9,10 @@ if (/^bot/i.test(m.text)) {
 conn.reply(m.soy, `*👻 ¡Hola! Soy VegetaBot en que puedo ayudarte hoy sabandija?  \n\n✰ Usa *.menu* para ver mis comandos insecto.*`, m, rcanal, )
 }
 
-if (/^reglas/i.test(m.text)) {
-conn.reply(m.chat, `*REGLAS DEL GRUP❍ꪜ* \n\n 📸 *Presentarse*
+if (/^Reglas/i.test(m.text)) {
+conn.reply(m.soy, `*REGLAS DEL GRUP❍ꪜ* \n\n 📸 *Presentarse*
 🚫𝗡𝗼 𝗘𝗻𝘃𝗶𝗮𝗿 𝗣𝗩 𝘀𝗶𝗻 𝗽𝗲𝗿𝗺𝗶𝘀𝗼
 🚫𝗡𝗼 𝘃𝗶𝗱𝗲𝗼🎥 𝗣𝗼𝗿𝗻𝗼𝗴𝗿𝗮𝗳𝗶𝗮 𝗜𝗻𝗳𝗮𝗻𝘁𝗶𝗹 𝘆 𝗱𝗲 𝗮𝗱𝘂𝗹𝘁𝗼 \n\n ━━━━━━V͇̿I͇̿P͇̿━━━━━━\n\n
 🔰🅿🆁🅾🅷🅸🅱🅸🅳🅾⚜️𝗡𝗼\n\n 𝗣𝗼𝗿𝗻𝗼𝗴𝗿𝗮𝗳𝗶𝗮\n\n➬⃢⃞⃟🔞𝗡𝗼 𝗺𝗲𝗻𝗼𝗿𝗲𝘀 𝗱𝗲 16 años\n\n➬⃢⃞⃟🩸𝗡𝗼 𝘃𝗶𝗱𝗲𝗼𝘀 𝗦𝗮𝗻𝗴𝗿𝗶𝗲𝗻𝘁𝗼𝘀\n\n➬⃢⃞⃟🚫𝗡𝗼 𝗣𝗼𝗿𝗻𝗼𝗴𝗿𝗮𝗳𝗶𝗰𝗼𝘀\n\n➬⃢⃞⃟❌𝗡𝗼 𝗠𝗮𝗻𝗱𝗮𝗿 𝗣𝗩 𝘀𝗶𝗻 𝗽𝗲𝗿𝗺𝗶𝘀𝗼\n\n ➬⃢⃞⃟👀𝗡𝗼 𝗺𝗶𝗿𝗼𝗻𝗲𝘀\n\n➬⃢⃞⃟👾𝗡𝗼 𝘀𝗼𝗽𝗹𝗼𝗻𝗲𝘀\n\n➬⃢⃞⃟👻𝗡𝗼 𝗳𝗮𝗻𝘁𝗮𝘀𝗺𝗮\n\n➬⃢⃞⃟📱🚫𝗡𝗼 𝗦𝗽𝗮𝗺
-➬⃢⃞⃟🦠𝗩𝗶𝗿𝘂𝘀 𝘆 𝗧𝗿𝗮𝗯𝗮𝘀\n\n🚫NO ENLACES 🔗\n\n➬⃢⃞⃟💣𝗦𝗶 𝗻𝗼 𝗰𝘂𝗺𝗽𝗹𝗲 𝘁𝗲 𝗱𝗮𝗻 𝗕𝗮𝗺💣\n\n█║║██║║██║║██║║██║║█\n\n✧･ﾟ: *✧･Atte.\n\n☆ ፝͜★ৡ͜͡✞Saiyan➵͡☠️⃪̸ੵ᷒ᰰ↱\n\n✧･ﾟ: *✧･ﾟ:*✧･ﾟ: *✧･ﾟ:*✧･ﾟ: *✧･ﾟ:*`, m, rcanal, )
+➬⃢⃞⃟🦠𝗩𝗶𝗿𝘂𝘀 𝘆 𝗧𝗿𝗮𝗯𝗮𝘀\n\n🚫NO ENLACES 🔗\n\n➬⃢⃞⃟💣𝗦𝗶 𝗻𝗼 𝗰𝘂𝗺𝗽𝗹𝗲 𝘁𝗲 𝗱𝗮𝗻 𝗕𝗮𝗺💣\n\n█║║██║║██║║██║║██║║█\n\n✧･ﾟ: *✧･Atte.\n\n☆ ፝͜★ৡ͜͡✞Saiyan➵͡☠️⃪̸ੵ᷒ᰰ↱\n\n✧･ﾟ: *✧･ﾟ:*✧･ﾟ: *✧･ﾟ:*✧･ﾟ: *✧*`, m, rcanal, )
 }
