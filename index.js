9965f49101c9d5c3f38b9e55883f74910e0fea5c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-10 15:17:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Kakaroto ✰');
+console.log('\n✰ Iniciando BROOK-BOT-MD ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Kakaroto-Bot', {
+say('BROOK-BOT-MD', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • David-Chian`, {
+say(`Developed By • TEAM ANIME WARRIORS, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
