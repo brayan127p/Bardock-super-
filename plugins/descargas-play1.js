c1c58229d5bc282002bc1a1f55c36ca8a5521bec
Niño Piña <ninopina10@gmail.com>
2024-11-27 07:28:08 -0600

    Actualizar descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -33,8 +33,8 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     }, { quoted: m });
 }
 
-handler.command = ['play1', 'play2'];
-handler.help = ['play1', 'play2'];
+handler.command = ['play1', 'playv1'];
+handler.help = ['play1', 'playv1'];
 handler.tags = ['descargas'];
 export default handler;
 
