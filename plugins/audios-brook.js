76c42ceba06bc917d85819f23bfed26a1699d1a2
Niño Piña <ninopina10@gmail.com>
2024-11-26 20:30:24 -0600

    Actualizar plugins

---
+++ plugins/audios-brook.js
@@ -0,0 +1,7 @@
+let handler = asycn (m, { conn, usedPrefix, command }) => {
+let audio = fs.readFileSync('./brook/brook.mp3') // Cambia el directorio segun el audio
+await conn.sendFile(m.chat, audio, 'brook.mp3', '', m)
+}
+
+handler.command = ['brook3']
+export default handler
